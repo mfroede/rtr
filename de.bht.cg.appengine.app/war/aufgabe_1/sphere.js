e33cbcb0df86e50a198f046b584dd4556d499ea7
mfroede <marcus.froede@gmx.de>
2013-04-22 13:01:34 +0200

    refactoring sphere

--- de.bht.cg.appengine.app/war/aufgabe_1/sphere.js
+++ de.bht.cg.appengine.app/war/aufgabe_1/sphere.js
@@ -11,14 +11,14 @@ var Sphere = function (gl, depth) {
 } 
 
 Sphere.prototype.normalisedMiddle = function(a, b) {
-    var c = [(a[0]+b[0])/2.0,(a[1]+b[1])/2.0,(a[2]+b[2])/2.0];
+    var c = [(a[0] + b[0]) / 2.0,
+             (a[1] + b[1]) / 2.0,
+             (a[2] + b[2]) / 2.0];
     vec3.normalize(c);
     return c;
 };
 
 Sphere.prototype.configVertices = function(depth, a, b, c) {
-
-
 	if(depth == 0){
     	this.addVertex( a, b, c)
 		this.addColor(  a, b, c);
@@ -29,10 +29,10 @@ Sphere.prototype.configVertices = function(depth, a, b, c) {
     var bc = this.normalisedMiddle(b, c);
     var ca = this.normalisedMiddle(c, a);
 
-    this.configVertices(depth -1, ab, a, ca);
-    this.configVertices(depth -1, ab, b, bc);
-    this.configVertices(depth -1, bc, c, ca);
-    this.configVertices(depth -1, ab, bc, ca);
+    this.configVertices(depth - 1, ab, a, ca);
+    this.configVertices(depth - 1, ab, b, bc);
+    this.configVertices(depth - 1, bc, c, ca);
+    this.configVertices(depth - 1, ab, bc, ca);
 }
 
 Sphere.prototype.addVertex = function(a, b, c) {
@@ -52,8 +52,6 @@ Sphere.prototype.addColor = function(a, b, c) {
 }
 
 Sphere.prototype.initBuffers = function() {
-    this.vertexPositionBuffer = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexPositionBuffer);
 
     var positonA = [-1.0,-1.0, 1.0];
     var positonB = [ 1.0,-1.0,-1.0];
@@ -70,13 +68,14 @@ Sphere.prototype.initBuffers = function() {
     this.configVertices(this.depth, positonA, positonC, positonD);
     this.configVertices(this.depth, positonC, positonD, positonB);
     
+    this.vertexPositionBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexPositionBuffer);
     this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.vertices), this.gl.STATIC_DRAW);
     this.vertexPositionBuffer.itemSize = 3;
     this.vertexPositionBuffer.numItems = this.vertices.length / this.vertexPositionBuffer.itemSize;
-
+    
     this.colorvertexColorBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorvertexColorBuffer);
-    
     this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.colors), this.gl.STATIC_DRAW);
     this.colorvertexColorBuffer.itemSize = 4;
     this.colorvertexColorBuffer.numItems = this.colors.length / this.colorvertexColorBuffer.itemSize;
