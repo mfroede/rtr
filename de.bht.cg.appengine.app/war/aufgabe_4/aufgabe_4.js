0a5e892bcf9bda2d5880bc0ca7261b863e6f8fcb
mfroede <marcus.froede@gmx.de>
2013-05-26 11:15:33 +0200

    start Skybox

--- de.bht.cg.appengine.app/war/aufgabe_4/aufgabe_4.js
+++ de.bht.cg.appengine.app/war/aufgabe_4/aufgabe_4.js
@@ -50,6 +50,19 @@ function StringToVec3(string, defaulColor) {
    return vec3.create([ x, y, z ]);
 };
 
+function setupSkybox(SkyTextures, SkyProgram) {
+   var arrays = tdl.primitives.createCube(10);
+   tdl.primitives.reorientPositions(arrays.position, mat4.scale(mat4.identity([]), [-1, -1, -1]));
+
+//   tdl.primitives.reorient(arrays,
+//					       [1, 0, 0, 0,
+//					        0, 0, 1, 0,
+//					        0, 1, 0, 0,
+//					        0, 0, 0.9999, 1]);
+   return new tdl.models.Model(SkyProgram[0], arrays, SkyTextures);
+};
+
+
 // The main entry point.
 function initialize() {
    // Setup the canvas widget for WebGL.
@@ -82,9 +95,6 @@ function initialize() {
 
    var frag = window.location.hash.substring(1);
    var pnum = frag ? parseInt(frag) : 0;
-
-   var edgelength = document.getElementById("cubelength").value == 0 ? 0.7
-         : document.getElementById("cubelength").value;
 
    var torus = new tdl.models.Model(programs[pnum], tdl.primitives.createTorus(0.3, 0.15, 60, 60), textures);
    var circleTexture = false;
@@ -126,69 +136,71 @@ function initialize() {
       document.getElementById("light_" + i + "_i").onchange = readLights;
    }
 
-   var changeCircleTexture = function() {
-      torusConst.radius = document.getElementById("circleradius").value == 0 ? radius : document
-            .getElementById("circleradius").value;
-      torusConst.number = document.getElementById("circlenumber").value == 0 ? number : document
-            .getElementById("circlenumber").value;
-      torusConst.circleColor = StringToVec3(document.getElementById("circlecolor").value, circleColor);
-      torusConst.backround = StringToVec3(document.getElementById("cubecolor").value, backround);
-      torusConst.circleTexture = circleTexture;
-   };
-
-   document.getElementById("circlenumber").onchange = changeCircleTexture;
-   document.getElementById("circleradius").onchange = changeCircleTexture;
-   document.getElementById("circlecolor").onchange = changeCircleTexture;
-   document.getElementById("cubecolor").onchange = changeCircleTexture;
-
+   var eyePosition = vec3.create([0.0,0.0,-2.0]);
+   var viewRotateMatrix = mat4.create();
+   var target = vec3.create([0.0,0.0,1.0]);
+   var up = vec3.create([0.0,1.0,0.0]);
+   
    // Register a keypress-handler for shader program switching using the number
    // keys.
    window.onkeypress = function(event) {
       var n = String.fromCharCode(event.which);
-      if (n == "s") {
+      if (n == "q") {
          animate = !animate;
       } else if (n == "y") {
-         torus.setBuffers(tdl.primitives.createCube(edgelength), textures);
+         torus.setBuffers(tdl.primitives.createCube(0.75), textures);
       } else if (n == "x") {
          torus.setBuffers(tdl.primitives.createTorus(0.3, 0.15, 60, 60), textures);
       } else if (n == "c") {
-         torus.setBuffers(tdl.primitives.createSphere(0.4, 60, 60), textures);
-      } else if (n == "a") {
-         circleTexture = !circleTexture;
-         changeCircleTexture();
-      } else if (n == "q") {
-         torus.setProgram(programs[0]);
+         torus.setBuffers(tdl.primitives.createSphere(0.45, 60, 60), textures);
       } else if (n == "w") {
-         torus.setProgram(programs[1]);
-      } else if (n == "e") {
-         torus.setProgram(programs[2]);
-      } else if (n == "r") {
-         torus.setProgram(programs[3]);
-      } else if (n == "t") {
-         torus.setProgram(programs[4]);
-      } else if (n == "z") {
-         torus.setProgram(programs[5]);
-      } else if (n == "u") {
-         torus.setProgram(programs[6]);
-      }
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.translate(viewRotateMatrix, [0, 0, 0.5]);
+          mat4.multiply(viewRotateMatrix, view, view);
+      } else if (n == "a") {
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.translate(viewRotateMatrix, [-0.5, 0, 0]);
+          mat4.multiply(viewRotateMatrix, view, view);
+      } else if (n == "s") {
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.translate(viewRotateMatrix, [0, 0, -0.5]);
+          mat4.multiply(viewRotateMatrix, view, view);
+      } else if (n == "d") {
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.translate(viewRotateMatrix, [0.5, 0, 0]);
+          mat4.multiply(viewRotateMatrix, view, view);
+      } else if (n == "W") {
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.rotate(viewRotateMatrix, degToRad(1), [1, 0, 0]);
+          mat4.multiply(viewRotateMatrix, view, view);
+      } else if (n == "S") {
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.rotate(viewRotateMatrix, degToRad(-1), [1, 0, 0]);
+    	  mat4.multiply(viewRotateMatrix, view, view);
+      } else if (n == "A") {
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.rotate(viewRotateMatrix, degToRad(1), [0, 1, 0]);
+          mat4.multiply(viewRotateMatrix, view, view);
+      } else if (n == "D") {
+    	  mat4.identity(viewRotateMatrix);
+    	  mat4.rotate(viewRotateMatrix, degToRad(-1), [0, 1, 0]);
+          mat4.multiply(viewRotateMatrix, view, view);
+      } 
    };
 
    // Create some matrices and vectors now to save time later.
    var projection = mat4.create();
    var view = mat4.create();
+   mat4.lookAt(eyePosition, target, up, view);
    var model = mat4.create();
 
    // Uniforms for lighting.
-   var color = vec3.create();
+   var color = vec3.create([1.0,0.0,0.0]);
    var lights = [];
-
-   var eyePosition = vec3.create();
-   var target = vec3.create();
-   var up = vec3.create([ 0, 1, 0 ]);
 
    // Animation parameters for the rotating eye-point.
    var eyeSpeed = 0.2;
-   var eyeHeight = 3;
+   var eyeHeight = 0;
    var eyeRadius = 3.5;
    var animate = false;
 
@@ -196,11 +208,8 @@ function initialize() {
    var elapsedTime = 0.0;
    var then = 0.0;
    var clock = 0.0;
-
-   var radius = document.getElementById("circleradius").value;
-   var number = document.getElementById("circlenumber").value;
-   var circleColor = StringToVec3(document.getElementById("circlecolor").value, circleColor);
-   var backround = StringToVec3(document.getElementById("cubecolor").value, backround);
+   
+   var skybox = setupSkybox(textures, programs);
 
    // Uniform variables that are the same for all torus in one frame.
    var torusConst = {
@@ -209,12 +218,7 @@ function initialize() {
       eyePosition : eyePosition,
       lightPositions : lightPositions,
       lightIntensities : lightIntensities,
-      time : clock,
-      radius : radius,
-      number : number,
-      circleColor : circleColor,
-      backround : backround,
-      circleTexture : circleTexture
+      time : clock
    };
 
    // Uniform variables that change for each torus in a frame.
@@ -222,6 +226,19 @@ function initialize() {
       model : model,
       color : color
    };
+   
+   var skymodel = mat4.create();
+   var invertView = mat4.create();
+   mat4.inverse(view, invertView);
+   var skyConst = {
+		   //view : invertView
+		   
+   };
+   var skyPer = { 
+	  model : skymodel, 
+	  color : color 
+   };
+   
 
    // Renders one frame and registers itself for the next frame.
    function render() {
@@ -239,16 +256,15 @@ function initialize() {
       }
 
       // Calculate the current eye position.
-      eyePosition[0] = Math.sin(clock * eyeSpeed) * eyeRadius;
-      eyePosition[1] = eyeHeight;
-      eyePosition[2] = Math.cos(clock * eyeSpeed) * eyeRadius;
+      target[1] = Math.sin(clock * eyeSpeed) * 4.0;
+      target[2] = Math.cos(clock * eyeSpeed) * 4.0;
 
       // Setup global WebGL rendering behavior.
       gl.enable(gl.BLEND);
       gl.viewport(0, 0, canvas.width, canvas.width * 0.6);
       gl.colorMask(true, true, true, true);
       gl.depthMask(true);
-      gl.clearColor(0.5, 0.5, 0.5, 1);
+      gl.clearColor(0.5, 0.5, 0.5, 1.0);
       gl.clearDepth(1);
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
 
@@ -256,25 +272,22 @@ function initialize() {
       gl.enable(gl.DEPTH_TEST);
 
       // Calculate the perspective projection matrix.
-      mat4.perspective(60, canvas.clientWidth / canvas.clientHeight, 0.1, 10, projection);
+      mat4.perspective(60, canvas.clientWidth / canvas.clientHeight, 0.1, 20, projection);
 
       // Calculate the viewing transfomation.
-      mat4.lookAt(eyePosition, target, up, view);
 
+      mat4.identity(skyPer.model)
+      skybox.drawPrep(skyConst);
+      skybox.draw(skyPer);
+      
       // Prepare rendering of toruss.
       torusConst.time = clock;
-      torus.drawPrep(torusConst);
 
-      var across = 3;
-      var half = (across - 1) / 2.0;
-
-      mat4.translate(mat4.identity(torusPer.model), [ 0, 0, 0 ]);
-      mat4.scale(torusPer.model, [ 3.0, 3.0, 3.0 ]);
+      //mat4.translate(mat4.identity(torusPer.model), [ 0, 0, 0 ]);
+      mat4.scale(mat4.identity(torusPer.model), [ 1.0, 1.0, 1.0 ]);
       mat4.multiply(torusPer.model, moonRotationMatrix);
-      torusPer.color[0] = 1;
-      torusPer.color[1] = 0;
-      torusPer.color[2] = 0;
 
+      torus.drawPrep(torusConst);
       // Actually render one torus.
       torus.draw(torusPer);
    }
